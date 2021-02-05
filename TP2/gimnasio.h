47f92c6d9a4a6576863c889ff599f46ce1a5f57b
Borja Garibotti <mgaribotti@fi.uba.ar>
2021-02-04 22:32:24 -0300

    gimnasios started

---
+++ TP2/gimnasio.h
@@ -0,0 +1,12 @@
+#ifndef __GIMNASIO_H_
+#define __GIMNASIO_H_
+
+typedef struct gimnasio gimnasio_t;
+
+/**
+ * Lee información del gimnasio.
+ * TODO: donde la guarda?
+ */
+int cargar_gimnasio(char** ruta_archivo);
+
+#endif /*__GIMNASIO_H_*/
